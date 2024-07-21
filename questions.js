7868db263ee7fb61f1886989372bbaf337ad1081
subrata pal <palsubratappal@gmail.com>
2024-07-21 08:16:45 -0700

    First Commit
    First Date:21-07-2024

---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The register used to store the memory address location of the next instruction to be executed is called?",
+    answer: "program counter",
+    options: [
+      "flags",
+      "address register",
+      "program counter",
+      "accumulator"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many bit Program counter is available in 8085?",
+    answer: "16 bit",
+    options: [
+      "8 bit",
+      "16 bit",
+      "32 bit",
+      "4 bitprogram counter"
+    ]
+  },
+    {
+    numb: 3,
+    question: "what is the maximum addressing capability of Intel 8085?",
+    answer: "64 KB",
+    options: [
+      "64 KB",
+      "1 MB",
+      "4 KB",
+      "32 KB"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The main purpose of Accumulator register of 8085 is?",
+    answer: "temporary data storage",
+    options: [
+      "temporary data storage",
+      "selection of peripheral",
+      "storing instructions",
+      "used as primary pointer"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The 8085 Microprocessor has?",
+    answer: "8 - bit data bus 16 - bit address bus",
+    options: [
+      "8 - bit data bus 16 - bit address bus",
+      "8 - bit data bus 8 - bit address bus",
+      "16 - bit data bus 8 - bit address bus",
+      "16 - bit data bus 16 bit address bus"
+    ]
+  },
+];
